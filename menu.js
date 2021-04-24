58252d8a352a8d03561ccad24f68861629206c40
RobertasPetrokas <petrokas.robertas@gmail.com>
2021-04-24 23:54:13 +0300

    Initial commit

---
+++ menu.js
@@ -0,0 +1,119 @@
+var pizzaList = JSON.parse(sessionStorage.getItem("pizza_list"));
+var deleteBtn = document.getElementsByClassName("deleteButton");
+var pizzaHTML = document.getElementById("pizzaMenu");
+
+
+function createSortBtn(type, btnDiv) {
+    let btn = document.createElement("button");
+    btn.type = "button";
+    btn.classList.add("btn-group");
+    btnDiv.appendChild(btn);
+
+    btn.addEventListener("click", (() => {
+        pizzaList = sort(type);
+        pizzaHTML.innerHTML = "";
+        renderList();
+    }))
+    btn.appendChild(document.createTextNode("Sort by " + type));
+}
+
+function sort(prop) {
+    pizzaList.sort((a, b) => {
+        if (a[prop].toLowerCase() < b[prop].toLowerCase()) return -1;
+        else if (a[prop].toLowerCase() > b[prop].toLowerCase()) return 1;
+        else return 0;
+    });
+    return pizzaList;
+}
+
+function renderList() {
+
+    // Sort Buttons
+    let btnDiv = document.createElement("div");
+
+    createSortBtn("name", btnDiv);
+    createSortBtn("price", btnDiv);
+    createSortBtn("heat", btnDiv);
+    // btnDiv.classList.add('radio');
+    pizzaHTML.appendChild(btnDiv);
+
+    pizzaList.forEach(pizza => {
+
+        let pizzaProps = document.createElement("li");
+        pizzaProps.classList.add("box");
+
+        // Name
+        pizzaProps.appendChild(document.createElement("br"));
+        pizzaProps.appendChild(document.createTextNode("Name: " + pizza.name))
+
+        // Heat (Chilli peppers)
+        if (pizza.heat > 0) {
+            let heat = document.createElement("img")
+            heat.src = "/img/chili" + pizza.heat + ".png"
+            heat.classList.add("chilli")
+            pizzaProps.appendChild(heat);
+        }
+
+        // Price
+        let price = document.createElement("p");
+        price.innerText = "Price: " + pizza.price + " €";
+        pizzaProps.appendChild(price);
+
+
+
+        // Toppings
+        let toppings = document.createElement("p");
+        toppings.innerText = "Toppings: " + pizza.toppings;
+        pizzaProps.appendChild(toppings);
+
+        // Pizza photo
+        if (pizza.photo > 0) {
+            let img = document.createElement("img");
+            img.src = "/img/" + pizza.photo + ".png";
+            img.classList.add("pizzaMenuPhoto");
+            pizzaProps.appendChild(img);
+        }
+
+        // Additional Information
+        if (pizza.additionalInfo !== "") {
+            let additionalInfo = document.createElement("p");
+            additionalInfo.innerText = "Additional information: " + pizza.additionalInfo;
+            pizzaProps.appendChild(additionalInfo);
+            pizzaProps.appendChild(document.createElement("br"));
+        }
+
+        // Delete Button
+        let deleteBtn = document.createElement("button");
+        deleteBtn.appendChild(document.createTextNode("Delete"));
+        deleteBtn.classList.add("deleteButton");
+        pizzaProps.appendChild(document.createElement('br'));
+        pizzaProps.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener("click", () => {
+            if (confirm("Are you sure that you want to delete this pizza from the menu?")) {
+                var newMenu = pizzaList.filter(pizzaToRemove => {
+                    return pizzaToRemove != pizza
+                })
+                sessionStorage.setItem("pizza_list", JSON.stringify(newMenu));
+                pizzaHTML.removeChild(pizzaProps);
+            }
+        });
+
+        pizzaHTML.appendChild(pizzaProps);
+
+    });
+}
+
+
+if (pizzaList !== null) {
+    pizzaList = sort("name");
+    renderList();
+};
+
+let backBtn = document.createElement("button");
+backBtn.appendChild(document.createTextNode("<< Go back"));
+pizzaHTML.appendChild(backBtn);
+
+backBtn.addEventListener("click", () => {
+    window.location.replace("/index.html");
+});
---
+++ script.js
@@ -0,0 +1,92 @@
+var pizzaList = JSON.parse(sessionStorage.getItem("pizza_list"));
+var pizzaName = document.getElementById("pizzaName");
+var pizzaPrice = document.getElementById("pizzaPrice");
+var pizzaHeat = document.getElementById("pizzaHeat");
+var pizzaHeatOutput = document.getElementById("heatOutput");
+var pizzaAdditionalInfo = document.getElementById("additionalInfo");
+var pizzaPhoto = document.getElementById("pizzaPhoto");
+pizzaPhoto.value = 0;
+
+var toppingList = document.getElementById("toppingList");
+var pizzaToppings = document.getElementById("pizzaToppings");
+var toppings = [];
+
+
+function savePizza () {
+
+    if (pizzaName.value === "") {
+        alert("Please enter pizza name!");
+        return;
+    } else if (pizzaList !== null) {
+        var isDuplicate = false;
+        pizzaList.map(pizza => {
+            if (pizza.name === pizzaName.value) {
+                alert(pizza.name + " is already in the menu!");
+                isDuplicate = true;
+            }
+        });
+    }
+    if (pizzaPrice.value === null && !isDuplicate) {
+        alert("Please enter the price!");
+        return;
+    } else if (toppings.length < 2 && !isDuplicate) {
+        alert("Choose at least 2 toppings!");
+        return;
+    };
+
+
+    if (!isDuplicate) {
+        var pizza = {
+            name: pizzaName.value,
+            price: pizzaPrice.value,
+            heat: pizzaHeat.value,
+            toppings: toppings,
+            photo: pizzaPhoto.value,
+            additionalInfo: pizzaAdditionalInfo.value
+        };
+
+        if (pizzaList === null) {
+            sessionStorage.setItem("pizza_list", JSON.stringify([pizza]));
+        } else {
+            sessionStorage.setItem("pizza_list", JSON.stringify([...pizzaList, pizza]));
+        }
+
+        document.getElementById("addForm").reset();
+        pizzaHeatOutput.innerHTML = "0";
+        toppings = [];
+        toppingList.innerHTML = "";
+        pizzaPhoto.src = "/img/0.png";
+        pizzaPhoto.value = 0;
+    }
+}
+
+function addTopping () {
+
+    if (pizzaToppings.value !== "") {
+
+        toppings.push(pizzaToppings.value);
+        if (toppings.length === 1) {
+            toppingList.innerHTML = "Chosen toppings: " + pizzaToppings.value;
+        } else {
+            toppingList.innerHTML += (", " + pizzaToppings.value);
+        }
+        pizzaToppings.value = "";
+    }
+}
+
+pizzaHeat.oninput = function () {
+    pizzaHeatOutput.innerHTML = this.value;
+}
+
+function prevPhoto () {
+    pizzaPhoto.value--;
+    if (pizzaPhoto.value < 0) pizzaPhoto.value = 10;
+    pizzaPhoto.src = "/img/" + pizzaPhoto.value + ".png";
+    console.log(pizzaPhoto.value);
+}
+
+function nextPhoto () {
+    pizzaPhoto.value++;
+    if (pizzaPhoto.value > 10) pizzaPhoto.value = 0;
+    pizzaPhoto.src = "/img/" + pizzaPhoto.value + ".png";
+}
